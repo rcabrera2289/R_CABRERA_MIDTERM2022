7aed620a42cbc16758bd78d74da5a0c15191e9e9
rcabrera2289 <rebecca.cabrera222@gmail.com>
2022-12-26 22:06:53 -0500

    MakePyramid update

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,7 +1,13 @@
 package math.problems;
 
+import java.util.Scanner;
+
 public class MakePyramid {
     public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Whole Number of Stars: ");
+        int userInput= sc.nextInt();
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
@@ -14,6 +20,19 @@ public class MakePyramid {
 
         */
 
+        for (int i = 0; i <= userInput; i++) {
+            for (int j = userInput; j >= i ; j--) {
+                System.out.print(" ");
+
+            }
+            for (int k = 0; k <= i ; k++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+
 
         }
-  }
+
