a6054f2bd850886aefbca45aa9f136df9b49d65f
rcabrera2289 <rebecca.cabrera222@gmail.com>
2022-12-29 21:38:45 -0500

    UnitTestingMath update

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -4,8 +4,18 @@ public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
 
+        Factorial factorialObj= new Factorial();
+        int factorialNum= 5;
+        int result= (factorialObj.factorial(1));
+        System.out.println(result);
+
+
+        int factorialNum2= 10;
+        int result2= factorialObj.factor(10);
+        System.out.println(result2);
+
+
     }
+}
 
 
-
-        }
